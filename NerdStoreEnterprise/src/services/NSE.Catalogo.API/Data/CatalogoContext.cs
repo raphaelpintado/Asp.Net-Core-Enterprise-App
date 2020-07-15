8dc9bbd42ef72ebf9c78cfaa0370523fa124e3e1
Raphael Moraes <raphaelpintado@gmail.com>
2020-07-14 22:00:27 -0300

    API de Catalogo e Conceitos de Modelagem DDD

---
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Data/CatalogoContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using NSE.Core.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data
+{
+    public class CatalogoContext : DbContext, IUnitOfWork
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto>  Produtos { get; set; }        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var properties = modelBuilder.Model.GetEntityTypes().SelectMany(e => 
+                                e.GetProperties().Where(p => p.ClrType == typeof(string)));
+
+            foreach (var property in properties)
+            {
+                property.SetColumnType("varchar(100)");  //caso esqueça de setar alguma coluna string no Mapping
+            }
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Startup.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NSE.Catalogo.API.Data;
+using NSE.Catalogo.API.Models;
 
 namespace NSE.Catalogo.API
 {
@@ -25,7 +28,13 @@ namespace NSE.Catalogo.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CatalogoContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllers();
+
+            services.AddScoped<IProdutoRepository, IProdutoRepository>();
+            services.AddScoped<CatalogoContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
