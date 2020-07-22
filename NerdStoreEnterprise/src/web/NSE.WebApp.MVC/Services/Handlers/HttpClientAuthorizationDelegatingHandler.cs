583ae20a448197853ab7f63268f5dd53a3fca2ef
Raphael Moraes <raphaelpintado@gmail.com>
2020-07-22 19:59:06 -0300

    HttpService Catalogo e Delegating Handler

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,33 @@
+﻿using NSE.WebApp.MVC.Extensions;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services.Handlers
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IUser _user;
+
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = _user.ObterHttpContext().Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authorizationHeader))
+                request.Headers.Add("Authorization", new List<string> { authorizationHeader });
+
+            var token = _user.ObterUserToken();
+
+            if (!string.IsNullOrEmpty(token))
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
