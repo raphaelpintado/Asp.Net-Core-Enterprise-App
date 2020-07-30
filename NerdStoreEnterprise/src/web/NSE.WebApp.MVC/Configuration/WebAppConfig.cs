826d26c38142957e95cae7c2c5687067d626e60d
Raphael Moraes <raphaelpintado@gmail.com>
2020-07-30 19:39:13 -0300

    Adicionando a cultura

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.WebApp.MVC.Extensions;
+using System.Globalization;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -39,6 +41,14 @@ namespace NSE.WebApp.MVC.Configuration
             app.UseRouting();
 
             app.UseIdentityConfiguration();
+
+            var supportedCultures = new[] { new CultureInfo("pt-BR") };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("pt-BR"), 
+                SupportedCultures = supportedCultures, 
+                SupportedUICultures = supportedCultures
+            });
 
             app.UseMiddleware<ExceptionMiddleware>();
 
