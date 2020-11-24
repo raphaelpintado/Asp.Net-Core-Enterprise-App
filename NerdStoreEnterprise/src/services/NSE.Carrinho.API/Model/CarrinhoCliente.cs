167b24d1a38b0f7b03262b7414eb85adc7c168d1
raphaelpintado <raphael.moraes@cwi.com.br>
2020-11-24 18:37:10 -0300

    Comportamento de negócio da API de Carrinho

--- NerdStoreEnterprise/src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
+++ NerdStoreEnterprise/src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace NSE.Carrinho.API.Model
 {
@@ -19,5 +18,67 @@ namespace NSE.Carrinho.API.Model
         }
 
         public CarrinhoCliente() { }
+
+        internal void CalcularValorCarrinho()
+        {
+            ValorTotal = Itens.Sum(p => p.CalcularValor());
+        }
+
+        internal bool CarrinhoItemExistente(CarrinhoItem item)
+        {
+            return Itens.Any(p => p.ProdutoId == item.ProdutoId);
+        }
+
+        internal CarrinhoItem ObterPorProdutoId(Guid produtoId)
+        {
+            return Itens.FirstOrDefault(p => p.ProdutoId == produtoId);
+        }
+
+        internal void AdicionarItem(CarrinhoItem item)
+        {
+            if (!item.EhValido()) 
+                return;
+
+            item.AssociarCarrinho(Id);
+
+            if (CarrinhoItemExistente(item))
+            {
+                var itemExistente = ObterPorProdutoId(item.ProdutoId);
+                itemExistente.AdicionarUnidades(item.Quantidade);
+
+                item = itemExistente;
+                Itens.Remove(itemExistente);
+            }
+
+            Itens.Add(item);
+            CalcularValorCarrinho();
+        }
+
+        internal void AtualizarItem(CarrinhoItem item)
+        {
+            if (!item.EhValido())
+                return;
+
+            item.AssociarCarrinho(Id);
+
+            var itemExistente = ObterPorProdutoId(item.ProdutoId);
+
+            Itens.Remove(itemExistente);
+            Itens.Add(item);
+
+            CalcularValorCarrinho();
+        }
+
+        internal void AtualizarUnidades(CarrinhoItem item, int unidades)
+        {
+            item.AtualizarUnidades(unidades);
+            AtualizarItem(item);
+        }
+
+        internal void RemoverItem(CarrinhoItem item)
+        {
+            Itens.Remove(ObterPorProdutoId(item.ProdutoId));
+            CalcularValorCarrinho();
+        }
     }
 }
--- NerdStoreEnterprise/src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
+++ NerdStoreEnterprise/src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using System;
 
 namespace NSE.Carrinho.API.Model
 {
@@ -21,6 +19,57 @@ namespace NSE.Carrinho.API.Model
 
         public Guid CarrinhoId { get; set; }
 
-        public CarrinhoCliente CarrinhoCliente { get; set; }
+        public CarrinhoCliente CarrinhoCliente { get; set; }        
+
+        internal void AssociarCarrinho(Guid carrinhoId)
+        {
+            CarrinhoId = carrinhoId;
+        }
+
+        internal decimal CalcularValor()
+        {
+            return Quantidade * Valor;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal void AtualizarUnidades(int unidades)
+        {
+            Quantidade = unidades;
+        }
+
+        internal bool EhValido()
+        {
+            return new ItemPedidoValidation().Validate(this).IsValid;
+        }
+
+        public class ItemPedidoValidation : AbstractValidator<CarrinhoItem>
+        {
+            public ItemPedidoValidation()
+            {
+                RuleFor(c => c.ProdutoId)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do produto inválido");
+
+                RuleFor(c => c.Nome)
+                    .NotEmpty()
+                    .WithMessage("O nome do produto não foi informado");
+
+                RuleFor(c => c.Quantidade)
+                    .GreaterThan(0)
+                    .WithMessage("A quantidade mínima de um item é 1");
+
+                RuleFor(c => c.Quantidade)
+                    .LessThan(5)
+                    .WithMessage("A quantidade máxima de um item é 5");
+
+                RuleFor(c => c.Valor)
+                    .GreaterThan(0)
+                    .WithMessage("O valor do item precisa ser maior que 0");
+            }
+        }
     }
 }
