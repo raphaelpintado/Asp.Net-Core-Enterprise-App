5f73c152d74a0757796ede068f84cb4764c471a0
raphaelpintado <raphael.moraes@cwi.com.br>
2020-12-07 16:48:49 -0300

    consumindo serviços pelo BFF

--- NerdStoreEnterprise/src/building blocks/NSE.WebAPI.Core/Controllers/MainController.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.WebAPI.Core/Controllers/MainController.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NSE.Core.Communication;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -43,6 +44,26 @@ namespace NSE.WebAPI.Core.Controllers
             }
 
             return CustomResponse();
+        }
+
+        protected ActionResult CustomResponse(ResponseResult resposta)
+        {
+            ResponsePossuiErros(resposta);
+
+            return CustomResponse();
+        }
+
+        protected bool ResponsePossuiErros(ResponseResult resposta)
+        {
+            if (resposta == null || !resposta.Errors.Mensagens.Any())
+                return false;
+
+            foreach (var mensagem in resposta.Errors.Mensagens)
+            {
+                AdicionarErroProcessamento(mensagem);
+            }
+
+            return true;
         }
 
         protected bool OperacaoValida()
---
+++ NerdStoreEnterprise/src/building blocks/NSE.WebAPI.Core/Extensions/PollyExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
+using System;
+using System.Net.Http;
+
+namespace NSE.WebAPI.Core.Extensions
+{
+    public static class PollyExtensions
+    {
+        public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
+        {
+            return HttpPolicyExtensions
+                        .HandleTransientHttpError()
+                        .WaitAndRetryAsync(new[]
+                        {
+                            TimeSpan.FromSeconds(1),
+                            TimeSpan.FromSeconds(5),
+                            TimeSpan.FromSeconds(10)
+                        });
+        }
+    }
+}
